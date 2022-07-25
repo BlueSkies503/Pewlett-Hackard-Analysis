a0ea4f538b081f11b769eb7e7a867552b3c93ec9
Hunter Matthews <huntermatthews@Hunters-MacBook-Pro.local>
2022-07-24 20:57:07 -0700

    Added Deliverable 2

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -29,9 +29,32 @@ INNER JOIN titles AS ti
 
 
 -- Create Retiring Titles table
-  SELECT COUNT(u.title),
-         u.title
-    INTO retiring_titles
-    FROM unique_title AS u
-GROUP BY u.title
-ORDER BY u.count DESC;
+    SELECT COUNT(u.title),
+           u.title
+      INTO retiring_titles
+      FROM unique_title AS u
+  GROUP BY u.title
+  ORDER BY u.count DESC;
+
+
+-- Deliverable 2 --------------
+-------------------------------
+
+-- Create mentorship_eligibilty.csv
+SELECT DISTINCT ON (e.emp_no)
+           e.emp_no,
+           e.first_name,
+           e.last_name,
+           e.birth_date,
+           de.from_date,
+           de.to_date,
+           ti.title
+      INTO mentor_elig
+      FROM employees AS e
+INNER JOIN dept_emp AS de
+        ON (e.emp_no = de.emp_no)
+INNER JOIN titles AS ti
+        ON (e.emp_no = ti.emp_no)
+     WHERE (de.to_date = '9999-01-01')
+       AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+  ORDER BY e.emp_no, ti.to_date DESC;
